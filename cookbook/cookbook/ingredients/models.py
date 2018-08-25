fed3d96fe6634d6066355fa990d98d82a1200562
wederribas <wederribas@gmail.com>
2018-08-25 19:21:12 -0300

    Fix typo in notes columns and apply migration

--- cookbook/cookbook/ingredients/models.py
+++ cookbook/cookbook/ingredients/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
-    nodes = models.TextField()
+    notes = models.TextField()
     category = models.ForeignKey(
         Category, related_name='ingredients', on_delete=models.CASCADE
     )
